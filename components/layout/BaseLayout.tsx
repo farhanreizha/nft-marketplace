470ff371be37a6f34c7645a94db8e5a0d681a2cd
farhanreizha <reizha226@gmail.com>
2022-06-02 20:48:42 +0700

    added Base Layout and move navbar to base layout component

---
+++ components/layout/BaseLayout.tsx
@@ -0,0 +1,19 @@
+import { ReactNode, FC } from 'react'
+import { Navbar } from 'components'
+
+interface Props {
+  children: ReactNode
+}
+
+const BaseLayout: FC<Props> = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <div className='py-16 bg-gray-50 overflow-hidden min-h-screen'>
+        <div className='max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8'>{children}</div>
+      </div>
+    </>
+  )
+}
+
+export default BaseLayout
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,8 @@
 import 'shared/globals.css'
 import type { AppProps } from 'next/app'
-import { Navbar } from 'components'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Navbar />
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
