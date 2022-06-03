9f08accc40fcba8a70c1f094bccaef2e63d34ee0
farhanreizha <reizha226@gmail.com>
2022-06-03 20:52:11 +0700

    make profile page and link profile

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -4,7 +4,6 @@ import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 import Link from 'next/link'
-import Image from 'next/image'
 
 const navigation = [
   { name: 'Marketplace', href: '/', current: true },
@@ -75,9 +74,7 @@ export default function Navbar() {
                   <div>
                     <Menu.Button className='bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'>
                       <span className='sr-only'>Open user menu</span>
-                      <Image
-                        height={32}
-                        width={32}
+                      <img
                         className='h-8 w-8 rounded-full'
                         src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
                         alt=''
@@ -96,41 +93,16 @@ export default function Navbar() {
                     <Menu.Items className='origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'>
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href='#'
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700'
-                            )}
-                          >
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href='#'
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700'
-                            )}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href='#'
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700'
-                            )}
-                          >
-                            Sign out
-                          </a>
+                          <Link href='/profile'>
+                            <a
+                              className={classNames(
+                                active ? 'bg-gray-100' : '',
+                                'block px-4 py-2 text-sm text-gray-700'
+                              )}
+                            >
+                              Your Profile
+                            </a>
+                          </Link>
                         )}
                       </Menu.Item>
                     </Menu.Items>
--- components/nft/item/index.tsx
+++ components/nft/item/index.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+/* eslint-disable @next/next/no-img-element */
 import { FC } from 'react'
 import { NftMeta } from 'types/nft'
 
@@ -10,13 +10,7 @@ const NftItem: FC<NftItemProps> = ({ item }) => {
   return (
     <>
       <div className='flex-shrink-0'>
-        <Image
-          height={600}
-          width={600}
-          className={`h-full w-full object-cover`}
-          src={item.image}
-          alt='New NFT'
-        />
+        <img className={`h-full w-full object-cover`} src={item.image} alt='New NFT' />
       </div>
       <div className='flex-1 bg-white p-6 flex flex-col justify-between'>
         <div className='flex-1'>
@@ -33,13 +27,7 @@ const NftItem: FC<NftItemProps> = ({ item }) => {
               <dd className='order-1 text-xl font-extrabold text-indigo-600'>
                 <div className='flex justify-center items-center'>
                   100
-                  <Image
-                    width={24}
-                    height={24}
-                    className='h-6'
-                    src='/images/small-eth.webp'
-                    alt='ether icon'
-                  />
+                  <img className='h-6' src='/images/small-eth.webp' alt='ether icon' />
                 </div>
               </dd>
             </div>
