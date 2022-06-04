79c21549ab6669559cb8e848553630f267705392
farhanreizha <reizha226@gmail.com>
2022-06-04 19:32:56 +0700

    move type web3 to utils and fixed null error on web3 context and initialstate

--- components/providers/web3/index.tsx
+++ components/providers/web3/index.tsx
@@ -1,21 +1,14 @@
 import { createContext, FC, ReactNode, useContext, useState } from 'react'
-import { MetaMaskInpageProvider } from '@metamask/providers'
-import { Contract, providers } from 'ethers'
+import { createDefaultState, Web3State } from './utils'
 
-export type Web3State = {
-  ethereum?: MetaMaskInpageProvider
-  provider?: providers.Web3Provider
-  contract?: Contract
-}
-
-const Web3Context = createContext<Web3State>(null)
+const Web3Context = createContext<Web3State>(createDefaultState())
 
 interface Props {
   children: ReactNode
 }
 
 const Web3Provider: FC<Props> = ({ children }) => {
-  const [web3Api, setWeb3Api] = useState<Web3State>(null)
+  const [web3Api, setWeb3Api] = useState<Web3State>(createDefaultState())
 
   return <Web3Context.Provider value={web3Api}>{children}</Web3Context.Provider>
 }
