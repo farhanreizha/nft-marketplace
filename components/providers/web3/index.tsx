78691f5dd7bf705417e45598fe0697647d06b3a8
farhanreizha <reizha226@gmail.com>
2022-06-04 19:13:42 +0700

    make web3 type and installed ethers and @metamask/providers

--- components/providers/web3/index.tsx
+++ components/providers/web3/index.tsx
@@ -1,13 +1,21 @@
 import { createContext, FC, ReactNode, useContext, useState } from 'react'
+import { MetaMaskInpageProvider } from '@metamask/providers'
+import { Contract, providers } from 'ethers'
 
-const Web3Context = createContext<any>(null)
+export type Web3State = {
+  ethereum?: MetaMaskInpageProvider
+  provider?: providers.Web3Provider
+  contract?: Contract
+}
+
+const Web3Context = createContext<Web3State>(null)
 
 interface Props {
   children: ReactNode
 }
 
 const Web3Provider: FC<Props> = ({ children }) => {
-  const [web3Api, setWeb3Api] = useState({ test: 'Hello Provider!' })
+  const [web3Api, setWeb3Api] = useState<Web3State>(null)
 
   return <Web3Context.Provider value={web3Api}>{children}</Web3Context.Provider>
 }
