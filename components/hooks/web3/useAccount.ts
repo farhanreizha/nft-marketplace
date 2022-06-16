83d0d2ff305e8b2f3ef5057a6b135870dc265861
farhanreizha <reizha226@gmail.com>
2022-06-16 16:28:44 +0700

    make account data

--- components/hooks/web3/useAccount.ts
+++ components/hooks/web3/useAccount.ts
@@ -4,13 +4,22 @@ import useSWR from 'swr'
 type AccountHookFactory = CryptoHookFactory<string, string>
 
 export type UseAccountHook = ReturnType<AccountHookFactory>
-export const hookFactory: AccountHookFactory = (deps) => (params) => {
-  const swrRes = useSWR('web3/useAccount', () => {
-    console.log(deps)
-    console.log(params)
-    // making request to get data
-    return 'Test User'
-  })
-
-  return swrRes
-}
+export const hookFactory: AccountHookFactory =
+  ({ provider }) =>
+  () => {
+    const swrRes = useSWR(
+      provider ? 'web3/useAccount' : null,
+      async () => {
+        const accounts = await provider!.listAccounts()
+        const account = accounts[0]
+        if (!account) {
+          throw 'Cannot retrive account! Please, connect to web3 wallet.'
+        }
+        return account
+      },
+      {
+        revalidateOnFocus: false,
+      }
+    )
+    return swrRes
+  }
