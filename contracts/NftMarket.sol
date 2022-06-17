7c037ff7561d649cd1974e99de66df5cf1ead604
farhanreizha <reizha226@gmail.com>
2022-06-17 15:36:20 +0700

    added package prettier for solidity

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -5,22 +5,22 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NftMarket is ERC721URIStorage {
-    using Counters for Counters.Counter;
+  using Counters for Counters.Counter;
 
-    Counters.Counter private _listedItems;
-    Counters.Counter private _tokenIds;
+  Counters.Counter private _listedItems;
+  Counters.Counter private _tokenIds;
 
-    constructor() ERC721("CreaturesNFT", "CNFT") {}
+  constructor() ERC721("CreaturesNFT", "CNFT") {}
 
-    function minToken(string memory tokenURI) public payable returns (uint) {
-        _tokenIds.increment();
-        _listedItems.increment();
+  function minToken(string memory tokenURI) public payable returns (uint) {
+    _tokenIds.increment();
+    _listedItems.increment();
 
-        uint newTokenId = _tokenIds.current();
+    uint newTokenId = _tokenIds.current();
 
-        _safeMint(msg.sender, newTokenId);
-        _setTokenURI(newTokenId, tokenURI);
+    _safeMint(msg.sender, newTokenId);
+    _setTokenURI(newTokenId, tokenURI);
 
-        return newTokenId;
-    }
+    return newTokenId;
+  }
 }
