f5531952d4af4a6acf7ddab2f1bb4c26e3adf722
farhanreizha <reizha226@gmail.com>
2022-06-20 19:32:30 +0700

    Use Owned Nfts

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,9 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
-
 import type { NextPage } from 'next'
-
-import nfts from 'content/meta.json'
-import { NftMeta } from 'types/nft'
+import { Nft } from 'types/nft'
+import { useOwnedNfts } from '@hooks/web3'
 
 const tabs = [{ name: 'Your Collection', href: '#', current: true }]
 
@@ -12,19 +10,26 @@ function classNames(...classes: string[]) {
 }
 
 const Profile: NextPage = () => {
+  const { nfts } = useOwnedNfts()
+
   return (
-    <div className='h-full flex'>
-      <div className='flex-1 flex flex-col overflow-hidden'>
-        <div className='flex-1 flex items-stretch overflow-hidden'>
-          <main className='flex-1 overflow-y-auto'>
-            <div className='pt-8 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
-              <div className='flex'>
-                <h1 className='flex-1 text-2xl font-bold text-gray-900'>Your NFTs</h1>
+    <div className="h-full flex">
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <div className="flex-1 flex items-stretch overflow-hidden">
+          <main className="flex-1 overflow-y-auto">
+            <div className="pt-8 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <div className="flex">
+                <h1 className="flex-1 text-2xl font-bold text-gray-900">
+                  Your NFTs
+                </h1>
               </div>
-              <div className='mt-3 sm:mt-2'>
-                <div className='hidden sm:block'>
-                  <div className='flex items-center border-b border-gray-200'>
-                    <nav className='flex-1 -mb-px flex space-x-6 xl:space-x-8' aria-label='Tabs'>
+              <div className="mt-3 sm:mt-2">
+                <div className="hidden sm:block">
+                  <div className="flex items-center border-b border-gray-200">
+                    <nav
+                      className="flex-1 -mb-px flex space-x-6 xl:space-x-8"
+                      aria-label="Tabs"
+                    >
                       {tabs.map((tab) => (
                         <a
                           key={tab.name}
@@ -45,13 +50,17 @@ const Profile: NextPage = () => {
                 </div>
               </div>
 
-              <section className='mt-8 pb-16' aria-labelledby='gallery-heading'>
+              <section className="mt-8 pb-16" aria-labelledby="gallery-heading">
                 <ul
-                  role='list'
-                  className='grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 md:grid-cols-4 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'
+                  role="list"
+                  className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 md:grid-cols-4 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8"
                 >
-                  {(nfts as NftMeta[]).map((nft) => (
-                    <li key={nft.name} onClick={() => {}} className='relative'>
+                  {(nfts.data as Nft[]).map((nft) => (
+                    <li
+                      key={nft.tokenId}
+                      onClick={() => {}}
+                      className="relative"
+                    >
                       <div
                         className={classNames(
                           true
@@ -61,19 +70,24 @@ const Profile: NextPage = () => {
                         )}
                       >
                         <img
-                          src={nft.image}
-                          alt=''
+                          src={nft.meta.image}
+                          alt=""
                           className={classNames(
                             true ? '' : 'group-hover:opacity-75',
                             'object-cover pointer-events-none'
                           )}
                         />
-                        <button type='button' className='absolute inset-0 focus:outline-none'>
-                          <span className='sr-only'>View details for {nft.name}</span>
+                        <button
+                          type="button"
+                          className="absolute inset-0 focus:outline-none"
+                        >
+                          <span className="sr-only">
+                            View details for {nft.meta.name}
+                          </span>
                         </button>
                       </div>
-                      <p className='mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none'>
-                        {nft.name}
+                      <p className="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">
+                        {nft.meta.name}
                       </p>
                     </li>
                   ))}
@@ -83,49 +97,57 @@ const Profile: NextPage = () => {
           </main>
 
           {/* Details sidebar */}
-          <aside className='hidden w-96 bg-white p-8 border-l border-gray-200 overflow-y-auto lg:block'>
-            {true && (
-              <div className='pb-16 space-y-6'>
+          <aside className="hidden w-96 bg-white p-8 border-l border-gray-200 overflow-y-auto lg:block">
+            {false && (
+              <div className="pb-16 space-y-6">
                 <div>
-                  <div className='block w-full aspect-w-10 aspect-h-7 rounded-lg overflow-hidden'>
-                    <img src={nfts[0].image} alt='' className='object-cover' />
+                  <div className="block w-full aspect-w-10 aspect-h-7 rounded-lg overflow-hidden">
+                    <img
+                      src={nfts.data[0].meta.image}
+                      alt=""
+                      className="object-cover"
+                    />
                   </div>
-                  <div className='mt-4 flex items-start justify-between'>
+                  <div className="mt-4 flex items-start justify-between">
                     <div>
-                      <h2 className='text-lg font-medium text-gray-900'>
-                        <span className='sr-only'>Details for </span>
+                      <h2 className="text-lg font-medium text-gray-900">
+                        <span className="sr-only">Details for </span>
                         {nfts[0].name}
                       </h2>
-                      <p className='text-sm font-medium text-gray-500'>{nfts[0].description}</p>
+                      <p className="text-sm font-medium text-gray-500">
+                        {nfts[0].description}
+                      </p>
                     </div>
                   </div>
                 </div>
                 <div>
-                  <h3 className='font-medium text-gray-900'>Information</h3>
-                  <dl className='mt-2 border-t border-b border-gray-200 divide-y divide-gray-200'>
+                  <h3 className="font-medium text-gray-900">Information</h3>
+                  <dl className="mt-2 border-t border-b border-gray-200 divide-y divide-gray-200">
                     {nfts[0].attributes.map((attr) => (
                       <div
                         key={attr.trait_type}
-                        className='py-3 flex justify-between text-sm font-medium'
+                        className="py-3 flex justify-between text-sm font-medium"
                       >
-                        <dt className='text-gray-500'>{attr.trait_type}: </dt>
-                        <dd className='text-gray-900 text-right'>{attr.value}</dd>
+                        <dt className="text-gray-500">{attr.trait_type}: </dt>
+                        <dd className="text-gray-900 text-right">
+                          {attr.value}
+                        </dd>
                       </div>
                     ))}
                   </dl>
                 </div>
 
-                <div className='flex'>
+                <div className="flex">
                   <button
-                    type='button'
-                    className='flex-1 bg-indigo-600 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                    type="button"
+                    className="flex-1 bg-indigo-600 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                   >
                     Download Image
                   </button>
                   <button
                     onClick={() => {}}
-                    type='button'
-                    className='flex-1 ml-3 bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                    type="button"
+                    className="flex-1 ml-3 bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                   >
                     Transfer?
                   </button>
