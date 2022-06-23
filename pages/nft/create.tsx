87d753ffffebe23b4328b601b1c4144048c251c4
farhanreizha <reizha226@gmail.com>
2022-06-23 20:29:13 +0700

    Get Image bytes

--- pages/nft/create.tsx
+++ pages/nft/create.tsx
@@ -21,6 +21,17 @@ const NftCreate: NextPage = () => {
     ]}
   )
 
+  const handleImage = async (e: ChangeEvent<HTMLInputElement>) => {
+    if(!e.target.files) {
+      console.log("Select a file");
+    }
+
+    const file = e.target.files[0]
+    const buffer = await file.arrayBuffer()
+    const bytes = new Uint8Array(buffer)
+    console.log(bytes);
+  }
+
   const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target
     setNftMeta({...nftMeta, [name]: value})
@@ -236,6 +247,7 @@ const NftCreate: NextPage = () => {
                             >
                               <span>Upload a file</span>
                               <input
+                                onChange={handleImage}
                                 id='file-upload'
                                 name='file-upload'
                                 type='file'
