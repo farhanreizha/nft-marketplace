9d18a59285c8bee93f44d1e8c797def027ad4c6a
farhanreizha <reizha226@gmail.com>
2022-06-29 21:25:38 +0700

    Handle promises

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,19 @@ import type { AppProps } from 'next/app'
 import { BaseLayout } from '@ui'
 import { Web3Provider } from '@providers'
 
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+  <>
+    <ToastContainer/>
     <Web3Provider>
       <BaseLayout>
         <Component {...pageProps} />
       </BaseLayout>
     </Web3Provider>
+  </>
   )
 }
 
